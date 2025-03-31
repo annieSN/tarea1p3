8225333b0aaa110a2f2f88a9db235d5a603a0150
Grace <glmpluz16@gmail.com>
2025-03-31 18:10:09 -0500

    ola

--- polynomial.cpp
+++ polynomial.cpp
@@ -1,8 +1,22 @@
 //
 // Created by rudri on 10/11/2020.
 //
-
 #include <iostream>
 #include "polynomial.h"
-using namespace std;
+#include <cassert>
+#include<cmath>
+using namespace ;
+
+class Polynomila{
+  friend void Poland (int x);
+  Polynomila(){};
+  Polynomila operator+(const Polynomila& otro) {
+    return Polynomila(x + otro.x, y + otro.y);
+}
+
+
+};
+
+
+
 
