9e294e6823b524c945b333697323fa15ac17fedb
Anne Saldarriaga <anniex.sn01@gmail.com>
2025-03-31 17:40:01 -0500

    ejercicio 1

--- vector_3d.h
+++ vector_3d.h
@@ -1,10 +1,30 @@
-//
-// Created by rudri on 10/11/2020.
-//
-
 #ifndef PROG3_FUNDAMENTOS_TASK_1_V2025_01_VECTOR_3D_H
 #define PROG3_FUNDAMENTOS_TASK_1_V2025_01_VECTOR_3D_H
 
+#include <iostream> 
 
+namespace geometry {
+namespace d3 {
 
-#endif //PROG3_FUNDAMENTOS_TASK_1_V2025_01_VECTOR_3D_H
+class vector_3d {
+private:
+    double x, y, z; 
+
+public:
+    vector_3d(double x = 0, double y = 0, double z = 0);
+
+    double get_x() const;
+    double get_y() const;
+    double get_z() const;
+
+    vector_3d operator+(const vector_3d& other) const;
+    vector_3d operator-(const vector_3d& other) const;
+    vector_3d& operator=(const vector_3d& other);
+
+    friend double dot_product(const vector_3d& v1, const vector_3d& v2);
+};
+
+}
+} 
+
+#endif // PROG3_FUNDAMENTOS_TASK_1_V2025_01_VECTOR_3D_H
